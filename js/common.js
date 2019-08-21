b418e0b304c20e15078face6121000e1e636d484
wang199406 <550914530@qq.com>
2019-08-22 00:23:03 +0800

    提交修改

---
+++ js/common.js
@@ -0,0 +1,14 @@
+(function(pp){
+    console.log('立即执行函数');
+    var ningjia = "立即执行函数参数";
+    console.log(ningjia);
+    var para = "参数";
+    console.log(pp);
+})(12);
+
+!function(para){
+    console.log("转化成立即执行函数");
+    console.log(ninjia);
+    console.log(para);
+    console.log("立即函数只执行自身的函数");
+}(10);
