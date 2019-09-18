793fbc51b515769ad3685f35213e23ea49ae28b3
wang199406 <550914530@qq.com>
2019-09-19 01:35:31 +0800

    测试class

--- js/class.js
+++ js/class.js
@@ -2,6 +2,8 @@ class Test{
 
     constructor(name){
         this.name = name;
+        this.age;
+        this.sex
     }
 
     // constructor(high,weight){
@@ -13,8 +15,9 @@ class Test{
     }
 
     print (print){
-
+        this.age = 10;
         console.log("实现打印的效果",this.name);
+        console.log("直接使用构造方法的属性"+this.age);
     }
 }
 
