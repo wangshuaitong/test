67926b9fca8a475de13b754e31e9062d133a4d6d
wang199406 <550914530@qq.com>
2019-11-13 23:16:01 +0800

    “添加信息

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 (function(pp){
     console.log('立即执行函数');
     var ningjia = "立即执行函数参数";
-    console.log(ningjia);
+    // /console.log(ningjia);
     var para = "参数";
     debugger;
     console.log(pp);
@@ -9,7 +9,7 @@
 
 !function(para){
     console.log("转化成立即执行函数");
-    console.log(ninjia);
+    // console.log(ninjia);
     console.log(para);
     console.log("立即函数只执行自身的函数");
 }(10);
