1a6dd89ce4dcc2e7c0022c1b91d4074b6fc83eef
wang199406 <550914530@qq.com>
2019-11-27 01:20:42 +0800

    test class property by me

---
+++ js/classTest.js
@@ -0,0 +1,13 @@
+class Controller{
+
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
+    onInit(){
+        alert("公用方法");
+    }
+    aler(){
+        alert("设置类的公用方法");
+    }
+}
